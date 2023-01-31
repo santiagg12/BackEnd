4f011240e167752c04699422cbdd696ffc869b8d
santiagg12 <santiirlanda@gmail.com>
2023-01-31 11:43:47 -0300

    m

--- src/main/java/com/portfolio/santiago/Controller/PersonaController.java
+++ src/main/java/com/portfolio/santiago/Controller/PersonaController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins = {"https://portfolio-morandi.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService ;
     
