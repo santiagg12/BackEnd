eee0ff1756641ba4b8d3985aea0b8bde96eac542
santiagg12 <santiirlanda@gmail.com>
2023-03-24 18:17:29 -0300

    gettersands

--- src/main/java/com/portfolio/santiago/Entity/Persona.java
+++ src/main/java/com/portfolio/santiago/Entity/Persona.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+
 @Entity
 public class Persona {
     @Id
@@ -37,6 +37,46 @@ public class Persona {
         this.nombre = nombre;
         this.apellido = apellido;
         this.descripcion = descripcion;
+        this.img = img;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
         this.img = img;
     }
 
