dd02d216ea3483f4d1c2c0f310653ea19a63f8e0
santiagg12 <santiirlanda@gmail.com>
2023-03-24 18:09:53 -0300

    t

--- src/main/java/com/portfolio/santiago/Entity/Persona.java
+++ src/main/java/com/portfolio/santiago/Entity/Persona.java
@@ -33,5 +33,12 @@ public class Persona {
     public Persona() {
     }
 
+    public Persona(String nombre, String apellido, String descripcion, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+
 }
 
